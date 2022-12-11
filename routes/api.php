be87ca32c3c8803706ad156d95ca59a8fe6d15c6
NAKNAO-nnct <totoro.windows@outlook.com>
2022-12-12 03:50:54 +0900

    install disk-s3 driver

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1', 'as' => 'auth.', 'middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
